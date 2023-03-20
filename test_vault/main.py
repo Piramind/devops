7beadacb296810de429b85a019365bf5fa7ce14c
Lev Tikhonov <ltikhonov@cloudlinux.com>
2023-03-20 18:14:51 +0300

    add userpass method and password gnerator

--- test_vault/main.py
+++ test_vault/main.py
@@ -1,23 +1,40 @@
 from venv import create
 import hvac
 import json
+import random
 
-
-token = client.auth.token.create(policies=['root'], ttl='1h')
-client.token = token
 client = hvac.Client(url='http://localhost:8200')
+token = client.auth.token.create(policies=['root'], ttl='1h')
 #client.auth.login(token)
 
+# enable userpass method
+client.sys.enable_auth_method(
+    method_type='userpass',
+    path='userpass-hvac',
+)
 
-def init_server():
-    print(f" Is client authenticated: {client.is_authenticated()}")
-init_server()
+
+# password generation
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+num = string.digits
+symbols = string.punctuation
+all = string.ascii_letters + string.digits + string.punctuation
+temp = random.sample(all, 8)
+pass_word = "".join(temp)
+
+# login with password
+client.auth.userpass.login(
+    username= input(),
+    password=pass_word,
+)
+
+#print(f" Is client authenticated: {client.is_authenticated()}") # auntification
 
 
-def create_secret():
-    create_response = client.secrets.kv.v2.create_or_update_secret(path='hello', secret=dict(foo="bar"))
-    print(json.dumps(create_response, indent=4, sort_keys=True))
-create_secret()
+create_response = client.secrets.kv.v2.create_or_update_secret(path='secret', secret=dict(foo="bar"))
+create_response = client.secrets.kv.v2.create_or_update_secret(path='hello', secret=dict(foo="bar"))
+print(json.dumps(create_response, indent=4, sort_keys=True))
 
 
 def create_policy():
